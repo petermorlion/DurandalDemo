0fd47da2f5407cd842d888a9ff9b1a1ccae19298
Peter Morlion <peter.morlion@live.be>
2014-01-02 11:46:40 +0100

    basic saving (localStorage) of beer, navigate back to overview

--- Html only demo/app/viewmodels/add.js
+++ Html only demo/app/viewmodels/add.js
@@ -1,11 +1,24 @@
-define(['knockout', 'models/Beer'], function (ko, Beer) {
+define(['knockout', 'models/Beer', 'plugins/router'], function (ko, Beer, router) {
     return {
         displayName: 'Add',
-        name: ko.observable(),
-        brewery: ko.observable(),
+        name: ko.observable(''),
+        brewery: ko.observable(''),
         save: function () {
             var beer = new Beer(this.name(), this.brewery());
-            console.log(beer.name + ' ' + beer.brewery);
+
+            if (!localStorage.beers) {
+                var beers = {
+                    items: [beer]
+                };
+
+                localStorage.beers = JSON.stringify(beers);
+            } else {
+                var beers = JSON.parse(localStorage.beers);
+                beers.items.push(beer);
+                localStorage.beers = JSON.stringify(beers);
+            }
+
+            router.navigate('#beers');
         }
     }
 });
