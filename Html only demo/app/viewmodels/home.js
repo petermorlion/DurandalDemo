8a08d1c8f26867508cec0a6404b286f7b5773cca
Peter Morlion <peter.morlion@live.be>
2014-01-15 19:04:57 +0100

    incomplete commit

--- Html only demo/app/viewmodels/home.js
+++ Html only demo/app/viewmodels/home.js
@@ -1,7 +1,8 @@
-﻿define(['knockout'], function (ko) {
+﻿define(['knockout', 'viewmodels/beer-summary'], function (ko, BeerSummary) {
     return function HomeViewModel() {
         var self = this;
         self.displayName = "Beer o'clock";
+
         self.isProfileComplete = ko.computed(function () {
             if (!localStorage) {
                 return true;
@@ -9,6 +10,7 @@
 
             return localStorage.profile != undefined && JSON.parse(localStorage.profile).name !== '';
         });
+
         self.name = ko.computed(function () {
             if (self.isProfileComplete()) {
                 return JSON.parse(localStorage.profile).name;
@@ -16,5 +18,9 @@
 
             return '';
         });
+
+        self.randomBeer = function () {
+            return new BeerSummary({ beerId: 1, name: 'test', brewery: 'Inbev' });
+        }
     };
 })
