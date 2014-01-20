092b1e7557306764d0c615271690586e8f8a9412
Peter Morlion <peter.morlion@live.be>
2014-01-20 18:34:29 +0100

    introduced singelton for requireJS to show how it's done

--- Html only demo/app/viewmodels/beer-detail.js
+++ Html only demo/app/viewmodels/beer-detail.js
@@ -1,21 +1,25 @@
-﻿define(['models/beerRepository', 'plugins/router'], function (beerRepository, router) {
-    return function beerDetailViewModel() {
-        var self = this;
-
-        // TODO: is it possible to pass an object to the viewmodel when navigating to a new viewmodel?
-        self.activate = function (id) {
+﻿define(['models/beerRepository', 'plugins/router', 'knockout'], function (beerRepository, router, ko) {
+    // this returns a singleton, so the viewmodel stays in memory
+    // because we use the activate method every time the view is activated (because we need the parameter from the url), 
+    // the data will be updated every time the view is activated, giving us the correct data.
+    //
+    // It makes little sense to keep this object in memory, but I've included it here to show it is possible.
+    return {
+        beerId: ko.observable(''),
+        displayName: ko.observable(''),
+        brewery: ko.observable(''),
+        activate: function (id) {
             id = parseInt(id);
             var beer = beerRepository.get(id);
             if (beer) {
-                self.beerId = beer.beerId;
-                self.displayName = beer.name;
-                self.brewery = beer.brewery;
+                this.beerId(beer.beerId);
+                this.displayName(beer.name);
+                this.brewery(beer.brewery);
             }
-        };
-
-        self.remove = function () {
-            beerRepository.delete(self.beerId);
+        },
+        remove: function () {
+            beerRepository.delete(this.beerId());
             router.navigate('#beers');
-        };
+        }
     };
 });
