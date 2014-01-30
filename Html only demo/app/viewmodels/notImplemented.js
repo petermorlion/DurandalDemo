98a423534c25b9c9c98b8434c6cc8a5ed9a82434
Peter Morlion <peter.morlion@live.be>
2014-01-30 07:35:48 +0100

    custom dialog box

---
+++ Html only demo/app/viewmodels/notImplemented.js
@@ -0,0 +1,12 @@
+define(['knockout', 'plugins/dialog'], function (ko, dialog) {
+    return function NotImplementedViewModel() {
+        var self = this;
+        self.email = ko.observable('');
+        self.close = function () {
+            if (self.email() !== '') {
+                console.log('Send email address to server: ' + self.email());
+            }
+            dialog.close(self);
+        };
+    }
+});
--- Html only demo/app/viewmodels/shell.js
+++ Html only demo/app/viewmodels/shell.js
@@ -1,10 +1,8 @@
-﻿define(['plugins/router', 'durandal/app'], function (router, app) {
+﻿define(['plugins/router', 'durandal/app', 'viewmodels/notImplemented'], function (router, app, NotImplemented) {
     return {
         router: router,
         search: function() {
-            //It's really easy to show a message box.
-            //You can add custom options too. Also, it returns a promise for the user's response.
-            app.showMessage('Search not yet implemented...');
+            app.showDialog(new NotImplemented());
         },
         activate: function () {
             router.map([
